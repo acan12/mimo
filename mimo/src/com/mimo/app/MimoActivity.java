2bbd97ded33d9c1db48d2052f0038e85b82ffa3c
arys <arys@10.185.97.90>
2011-07-26 04:54:28 +0700

    first step mimo project

---
+++ mimo/src/com/mimo/app/MimoActivity.java
@@ -0,0 +1,15 @@
+package com.mimo.app;
+
+import android.*;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MimoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        String x = "1";
+    }
+}
