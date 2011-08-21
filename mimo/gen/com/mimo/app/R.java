74e966e92ecd4ad540701957696dc571fbce50b8
aryx <arycancer@gmail.com>
2011-08-21 11:32:10 +0700

    provide all layout for application

--- mimo/gen/com/mimo/app/R.java
+++ mimo/gen/com/mimo/app/R.java
@@ -14,13 +14,36 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050000;
+        public static final int Button01=0x7f05000e;
+        public static final int Button02=0x7f05000c;
+        public static final int Button03=0x7f05000b;
+        public static final int Button04=0x7f05000a;
+        public static final int Button05=0x7f050009;
+        public static final int Button06=0x7f050008;
+        public static final int Button07=0x7f050007;
+        public static final int Button08=0x7f050006;
+        public static final int Button09=0x7f050005;
+        public static final int Button10=0x7f050004;
+        public static final int Button11=0x7f050003;
+        public static final int TextView01=0x7f050010;
+        public static final int button1=0x7f05000f;
+        public static final int editText1=0x7f050012;
+        public static final int linearLayout1=0x7f050002;
+        public static final int scrollView1=0x7f050001;
+        public static final int textView1=0x7f050000;
+        public static final int textView2=0x7f05000d;
+        public static final int textView3=0x7f050011;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int layout_activitylist=0x7f030000;
+        public static final int layout_dashboard=0x7f030001;
+        public static final int layout_detail=0x7f030002;
+        public static final int layout_login=0x7f030003;
+        public static final int layout_mapview=0x7f030004;
+        public static final int layout_schedule_fragment=0x7f030005;
+        public static final int main=0x7f030006;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
     }
 }
