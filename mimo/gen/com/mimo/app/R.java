dbf88b35ad27e6cfc0783c5da31bf3e2120d637d
aryx <arycancer@gmail.com>
2011-08-21 13:20:10 +0700

    add login layout

--- mimo/gen/com/mimo/app/R.java
+++ mimo/gen/com/mimo/app/R.java
@@ -25,14 +25,17 @@ public final class R {
         public static final int Button09=0x7f050005;
         public static final int Button10=0x7f050004;
         public static final int Button11=0x7f050003;
+        public static final int EditText01=0x7f050013;
+        public static final int LinearLayout01=0x7f050012;
         public static final int TextView01=0x7f050010;
+        public static final int TextView02=0x7f050011;
         public static final int button1=0x7f05000f;
-        public static final int editText1=0x7f050012;
+        public static final int editText1=0x7f050015;
         public static final int linearLayout1=0x7f050002;
         public static final int scrollView1=0x7f050001;
         public static final int textView1=0x7f050000;
         public static final int textView2=0x7f05000d;
-        public static final int textView3=0x7f050011;
+        public static final int textView3=0x7f050014;
     }
     public static final class layout {
         public static final int layout_activitylist=0x7f030000;
