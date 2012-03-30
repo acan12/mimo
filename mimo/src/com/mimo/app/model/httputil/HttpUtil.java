b0d2aae4fd4d878705c2fad266e2d7ef28fe53fe
aryx <arycancer@gmail.com>
2012-03-31 05:32:40 +0700

    clean up the code

--- mimo/src/com/mimo/app/model/httputil/HttpUtil.java
+++ mimo/src/com/mimo/app/model/httputil/HttpUtil.java
@@ -57,7 +57,6 @@ public class HttpUtil {
 
 		HttpResponse response = httpClient.execute(requestBase);
 		httpEntity = response.getEntity();
-
 		String responses = getApiResponseBody(httpEntity);
 
 		return responses;
@@ -80,49 +79,32 @@ public class HttpUtil {
 		}
 
 		InputStream instream = entity.getContent();
-
 		if (instream == null) {
 			return "";
 		}
-
 		if (entity.getContentLength() > Integer.MAX_VALUE) {
 			throw new IllegalArgumentException(
-
 			"HTTP entity too large to be buffered in memory");
 		}
-
 		String charset = getContentCharSet(entity);
 
 		if (charset == null) {
-
 			charset = HTTP.DEFAULT_CONTENT_CHARSET;
-
 		}
-
 		Reader reader = new InputStreamReader(instream, charset);
-
 		StringBuilder buffer = new StringBuilder();
 
 		try {
-
 			char[] tmp = new char[1024];
-
 			int l;
 
 			while ((l = reader.read(tmp)) != -1) {
-
 				buffer.append(tmp, 0, l);
-
 			}
-
 		} finally {
-
 			reader.close();
-
 		}
-
 		return buffer.toString();
-
 	}
 
 	private static String getContentCharSet(HttpEntity entity) {
@@ -130,27 +112,19 @@ public class HttpUtil {
 		if (entity == null) {
 			throw new IllegalArgumentException("HTTP entity may not be null");
 		}
-
 		String charset = null;
 
 		if (entity.getContentType() != null) {
 
 			HeaderElement values[] = entity.getContentType().getElements();
-
 			if (values.length > 0) {
 
 				NameValuePair param = values[0].getParameterByName("charset");
-
 				if (param != null) {
-
 					charset = param.getValue();
-
 				}
-
 			}
-
 		}
-
 		return charset;
 	}
 
